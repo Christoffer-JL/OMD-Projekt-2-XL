3b469181897657566d43b42a7f95f6efd254e04f
Racha Jamaleddine <jamaleddinerasha@gmail.com>
2023-05-05 17:21:35 +0200

    completed printstream() and implemented some methods in grid

--- xl/model/Grid.java
+++ xl/model/Grid.java
@@ -1,60 +1,102 @@
 
 package xl.model;
 
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Grid extends Observable {
 
-    public Grid() {
+		private Map<String, Cell> grid;
+		
+		
+		
+	    public Grid() {
+	    grid=new HashMap<String, Cell>();
+	  }
 
-    }
+	   
+	    public Cell getCell(String cellAddress) {
+	        if(grid.containsKey(cellAddress)) {
+	        	return grid.get(cellAddress);
+	        }
+			return null;
+	    }
 
-    public Cell getCell(String cellAddress) {
-        return null;
-    }
+	   
+	    public void clearAllCells() {
+	    	grid.clear();
+	    	//not sure how observable should be implemeneted.. we should somehow notify GUI of the changes done 
+	    	setChanged();
+	    	notifyObservers();
+	    }
+	  
+	 
 
-    public void clearAllCells() {
+	    public void clearCell(String cellAddress) {
+	     if(grid.containsKey(cellAddress)) {
+	    	 grid.remove(cellAddress);
+	    	setChanged();
+	    	notifyObservers();
+	     }
+	    }
 
-    }
+	    public String display(String cellAddress) {
+	    	if(getCell(cellAddress) == null) {
+	    		throw new NoSuchElementException();
+	    	}
+	        return getCell(cellAddress).getValueAsString();
+	    }
 
-    public void clearCell(String cellAddress) {
-   
-    }
+	    
+	    public String formula(String cellAddress) {
+	    	if(getCell(cellAddress) == null) {
+	    		throw new NoSuchElementException();
+	    	}
+	    	return getCell(cellAddress).getFormula();
+	    }
+	    
+	    
+	    
+	    public void newFormula(String cellAddress, String newFormula) {
 
-    public String display(String cellAddress) {
-        return "";
-    }
+	    	
+	    	
+	    	
+	    }
 
-    public String formula(String cellAddress) {
-        return "";
-    }
+	     
+	    
+	    public void loadFile(String fileName) {
+	    	try {
+				XLBufferedReader loadfile= new XLBufferedReader(fileName);
+				loadfile.load(grid);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+	    }
 
-    public void newFormula(String cellAddress, String newFormula) {
+	    
+	    public void saveFile(String fileName) {
+	    	try (XLPrintStream savefile = new XLPrintStream(fileName)) {
+				savefile.save(grid.entrySet());
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+	    }
 
-    }
 
-    public void loadFile(String fileName) {
+	    @Override
+	    public void setChanged() {
 
-    }
+	    }
 
-    public void saveFile(String fileName) {
+	    @Override
+	    public void notifyObservers(Object o) {
 
-    }
+	    }
 
-    @Override
-    public void addObserver(Observer ob) {
-
-    }
-
-    @Override
-    public void setChanged() {
-
-    }
-
-    @Override
-    public void notifyObservers(Object o) {
-
-    }
-
-}
+	}
--- xl/model/XLPrintStream.java
+++ xl/model/XLPrintStream.java
@@ -5,16 +5,14 @@ import java.io.PrintStream;
 import java.util.Map.Entry;
 import java.util.Set;
 
-// TODO move to another package
 public class XLPrintStream extends PrintStream {
 
     public XLPrintStream(String fileName) throws FileNotFoundException {
         super(fileName);
     }
 
-    // TODO Change Object to something appropriate
-    public void save(Set<Entry<String, Object>> set) {
-        for (Entry<String, Object> entry : set) {
+    public void save(Set<Entry<String, Cell>> set) {
+        for (Entry<String, Cell> entry : set) {
             print(entry.getKey());
             print('=');
             println(entry.getValue());
