bb0b833843c2bb8c6c3e578067a3b1d51f730f3e
Christoffer-JL <christoffer.johansson.lundqvist@outlook.com>
2023-05-08 11:18:59 +0200

    Tog bort onödiga imports

--- xl/gui/SlotLabels.java
+++ xl/gui/SlotLabels.java
@@ -5,8 +5,6 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Observable;
-import java.util.Observer;
 
 import javax.swing.SwingConstants;
 
