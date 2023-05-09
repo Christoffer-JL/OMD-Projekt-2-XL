00b5a8cf1daa03e70f8f56c256e979a5ec18d7ff
Racha Jamaleddine <jamaleddinerasha@gmail.com>
2023-05-09 18:41:42 +0200

    clearall

--- xl/gui/menu/ClearAllMenuItem.java
+++ xl/gui/menu/ClearAllMenuItem.java
@@ -3,15 +3,19 @@ package xl.gui.menu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JMenuItem;
+import xl.gui.Controller;
+
 
 class ClearAllMenuItem extends JMenuItem implements ActionListener {
+    private Controller controller;
 
-    public ClearAllMenuItem() {
+    public ClearAllMenuItem(Controller controller) {
         super("Clear all");
         addActionListener(this);
+        this.controller=controller;
     }
 
     public void actionPerformed(ActionEvent e) {
-        // TODO
+       controller.clearAllCells();
     }
 }
