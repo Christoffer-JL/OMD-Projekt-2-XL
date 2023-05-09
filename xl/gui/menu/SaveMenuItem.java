75fde1dbd9f5e0affa3fd4edc421b6b8b461ee71
Lea <lea.togotzes@googlemail.com>
2023-05-09 19:50:16 +0200

    added load and save in MenuItems

--- xl/gui/menu/SaveMenuItem.java
+++ xl/gui/menu/SaveMenuItem.java
@@ -1,18 +1,22 @@
 package xl.gui.menu;
 
 import java.io.FileNotFoundException;
+import xl.gui.Controller;
 import javax.swing.JFileChooser;
 import xl.gui.StatusLabel;
 import xl.gui.XL;
 
 class SaveMenuItem extends OpenMenuItem {
+    private Controller controller;
 
-    public SaveMenuItem(XL xl, StatusLabel statusLabel) {
+
+    public SaveMenuItem(XL xl, StatusLabel statusLabel, Controller controller) {
         super(xl, statusLabel, "Save");
+        this.controller=controller;
     }
 
     protected void action(String path) throws FileNotFoundException {
-        // TODO
+        controller.save(path);
     }
 
     protected int openDialog(JFileChooser fileChooser) {
--- xl/gui/menu/XLMenuBar.java
+++ xl/gui/menu/XLMenuBar.java
@@ -13,8 +13,8 @@ public class XLMenuBar extends JMenuBar {
     public XLMenuBar(XL xl, XLList xlList, StatusLabel statusLabel, Controller controller) {
         JMenu file = new JMenu("File");
         JMenu edit = new JMenu("Edit");
-        file.add(new SaveMenuItem(xl, statusLabel));
-        file.add(new LoadMenuItem(xl, statusLabel));
+        file.add(new SaveMenuItem(xl, statusLabel, controller));
+        file.add(new LoadMenuItem(xl, statusLabel, controller));
         file.add(new NewMenuItem(xl));
         file.add(new CloseMenuItem(xl, xlList));
         edit.add(new ClearMenuItem());
